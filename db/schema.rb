2a5ad2d45261ed12c4d2ee8393df784ddf8477fa
dpowers1 <dmnckpwrs@gmail.com>
2024-10-03 11:37:00 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_142824) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_213117) do
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
@@ -19,4 +19,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_142824) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
